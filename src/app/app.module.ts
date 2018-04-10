e81de0a5fc39f8e8c707bc3b9381fdcc2e0a108f
INTERGRUPO\jdboterog <jdboterog@intergrupo.com>
2018-04-10 13:34:51 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
+const appRoutes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: '', redirectTo: '/search', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
